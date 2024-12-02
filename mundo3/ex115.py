6c4b297fb12cd313a6f3e09f542fc5966e0b0264
Giordano <brunogiordano.rodrigues@gmail.com>
2024-12-02 18:16:49 -0300

    Correção do comentário do ex115

--- mundo3/ex115.py
+++ mundo3/ex115.py
@@ -1,5 +1,6 @@
 # Exercício Python 115a: Vamos criar um menu em Python, usando modularização.
 # Exercício Python 115b: Vamos ver como fazer acesso a arquivos usando o Python.
+# Exercício Python 115c: Vamos finalizar o projeto de acesso a arquivos em Python.
 from mundo3.ex115.sistema.sistema import Sistema
 
 Sistema.start()
